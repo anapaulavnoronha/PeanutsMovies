b54ea1bae87657e0fdd22daf8c21bb173793949f
Ana Paula <anapaulavnoronha@gmail.com>
2016-08-14 16:03:00 -0300

    Secao Filmes- Iniciando a funcionalidade de ver mais filmes

---
+++ js/secaoFilmes.js
@@ -0,0 +1,16 @@
+$('#botao-mais-filmes-next').click(mostraFilmes);
+$('#botao-mais-filmes-prev').click(mostraFilmes);
+
+function mostraFilmes() {
+    var filmes = $('.filmes');
+
+    if (filmes.hasClass('mostra-primeiros-filmes')) {
+        filmes.removeClass('mostra-primeiros-filmes');
+        filmes.addClass('mostra-outros-filmes');
+    } else if (filmes.hasClass('mostra-outros-filmes')) {
+        filmes.removeClass('mostra-outros-filmes');
+        filmes.addClass('mostra-primeiros-filmes');
+    }
+
+
+}
